93939a1c855763e4278b6ff1b19b91bb2f92bab3
amit.raut <amitraut11@gmail.com>
2024-02-28 17:27:10 -0600

    added new changes

--- ecs.tf
+++ ecs.tf
@@ -37,8 +37,8 @@ resource "aws_ecs_task_definition" "demo_app_task" {
   DEFINITION
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  memory                   = 512
-  cpu                      = 256
+  memory                   = 1024
+  cpu                      = 512
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 }
 
